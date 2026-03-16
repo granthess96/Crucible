1fca954a5033fb2c24d03d985fa30c15723f456f
Grant Hess <granthess96@gmail.com>
2026-03-15 20:10:01 -0500

    Adding gcc 13.2

---
+++ components/gcc/build.py
@@ -0,0 +1,17 @@
+# components/gcc/build.py
+from kiln.builders.base import AutotoolsBuild, BuildPaths
+
+class GCC(AutotoolsBuild):
+    name    = 'gcc'
+    version = '13.2.0'
+    deps    = ['gmp', 'mpfr', 'mpc', 'binutils']
+    source  = {
+        'url': 'https://ftp.gnu.org/gnu/gcc/gcc-13.2.0/gcc-13.2.0.tar.xz',
+    }
+    configure_args = [
+        '--enable-languages=c,c++',
+        '--disable-multilib',
+        '--enable-shared',
+        '--disable-werror',
+        '--with-system-zlib',
+    ]
