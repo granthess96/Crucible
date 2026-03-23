894ea2d302a40e3dfeeb18107b3502a63a362afb
Grant Hess <granthess96@gmail.com>
2026-03-22 22:50:39 -0500

    Added libxcrypt dependency for gcc

---
+++ components/libxcrypt/build.py
@@ -0,0 +1,20 @@
+from kiln.builders import AutotoolsBuild
+
+class Libxcrypt(AutotoolsBuild):
+    name    = 'libxcrypt'
+    version = '4.4.36'
+    deps    = ['glibc', 'linux-headers']
+    
+    source  = {
+        'url': 'https://github.com/besser82/libxcrypt/releases/download/v4.4.36/libxcrypt-4.4.36.tar.xz',
+    }
+
+    # --enable-obsolete-api is crucial if you are fixing a "crypt.h" 
+    # issue for a GCC build or legacy software.
+    configure_args = [
+        '--enable-hashes=all',
+        '--enable-obsolete-api=glibc',
+        '--disable-static',
+    ]
+
+    c_flags = ['-O2', '-Wno-error=unterminated-string-initialization' ]
